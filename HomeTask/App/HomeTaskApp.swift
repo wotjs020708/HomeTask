4885e2a284ceda0a672449747c214d57b695fa7c
JaeSunEo <wotjs020708@gmail.com>
2026-03-19 18:02:25 +0900

    [setting] project init

---
+++ HomeTask/App/HomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTaskApp.swift
+//  HomeTask
+//
+//  Created by 어재선 on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
