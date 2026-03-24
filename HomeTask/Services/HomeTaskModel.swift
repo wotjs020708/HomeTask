566d3753250dcfbfacbfffae3deb01bef757c53d
JaeSunEo <wotjs020708@gmail.com>
2026-03-24 14:11:08 +0900

    [feat/#1] HomeTaskModel 생성

---
+++ HomeTask/Services/HomeTaskModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTaskModel.swift
+//  HomeTask
+//
+//  Created by 어재선 on 3/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@Observable
+class HomeTaskModel {
+    private let modelContext: ModelContext
+    
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+    }
+    
+  //TODO: Model관 관련된 로직 작성
+}
